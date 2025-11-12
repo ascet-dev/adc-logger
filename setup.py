5b9a66ad786f8804bdaadf9b43554b07f617ca28
maxponomarev <maxin.ponomarev@mychili.id>
2025-11-12 21:49:26 +0300

    Update setup.py: fix typo, add Python 3.12 support and Typed classifier, add MANIFEST.in for py.typed

--- setup.py
+++ setup.py
@@ -42,7 +42,9 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: Python :: 3.11',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Typing :: Typed',
     ],
     zip_safe=False,
 )
